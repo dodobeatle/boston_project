b3124b5a3fed71cbf5a89388d5034cd65e5b1179
Pedro Araujo <dodobeatle@gmail.com>
2025-05-06 18:08:35 -0300

    some problems were solved

--- docker/init-sql/init.sql
+++ docker/init-sql/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE boston_project;
 CREATE USER "dodobeatle@gmail.com" WITH ENCRYPTED PASSWORD 'airbyte';
-GRANT ALL PRIVILEGES ON DATABASE mlops TO "dodobeatle@gmail.com";
+GRANT ALL PRIVILEGES ON DATABASE boston_project TO "dodobeatle@gmail.com";
 GRANT ALL ON SCHEMA public TO "dodobeatle@gmail.com";
 GRANT USAGE ON SCHEMA public TO "dodobeatle@gmail.com";
 ALTER DATABASE boston_project OWNER TO "dodobeatle@gmail.com";
